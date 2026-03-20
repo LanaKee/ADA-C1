a9fad5b4fedeab7c2ae50574a78d64196a5dfd6d
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-03-20 15:02:02 +0900

    feat: implement UI

--- pacemaker/ContentView.swift
+++ pacemaker/ContentView.swift
@@ -11,32 +11,36 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var items: [Item]
+    @State private var goal: String = ""
+    // @FocusState private var goalFieldIsFocused: Bool = false
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        Spacer()
+        Text("PaceMaker")
+            .font(.largeTitle)
+        Text("이루고 싶은걸 적어주세요")
+            .font(.headline)
+            .foregroundStyle(.gray)
+        HStack(spacing: 8) {
+            TextField("애플에 입사하기?", text: $goal)
+                .textInputAutocapitalization(.never)
+                .disableAutocorrection(true)
+
+            Button(action: addItem) {
+                Image(systemName: "arrow.up")
+                    .foregroundColor(.white)
+                    .padding(10)
+                    .background(Color.blue)
+                    .clipShape(RoundedRectangle(cornerRadius: 10 ))
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            .disabled(goal.isEmpty)
         }
+        .padding(12)
+        .background(Color(.systemGray6))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .padding(.horizontal, 16)
+
+        Spacer()
     }
 
     private func addItem() {
