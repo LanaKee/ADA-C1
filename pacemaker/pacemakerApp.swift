43aa2f29b9b26f9f48ccabd311fd42e660ceaf04
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-04-20 18:41:33 +0900

    refactor: remove unused ModelContainer from app entry point

--- pacemaker/pacemakerApp.swift
+++ pacemaker/pacemakerApp.swift
@@ -20,25 +20,11 @@ struct pacemakerApp: App {
 
     FirebaseApp.configure()
   }
-  
-  var sharedModelContainer: ModelContainer = {
-    let schema = Schema([
-      Item.self,
-    ])
-    let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-    
-    do {
-      return try ModelContainer(for: schema, configurations: [modelConfiguration])
-    } catch {
-      fatalError("Could not create ModelContainer: \(error)")
-    }
-  }()
-  
+
   var body: some Scene {
     WindowGroup {
       ContentView()
     }
     .modelContainer(for: GoalModel.self)
-    .modelContainer(sharedModelContainer)
   }
 }
