bb1eb18b2ac28e06bd91808a7d44c3dc4ba45a58
LanaKee <55011525+LanaKee@users.noreply.github.com>
2026-04-20 14:00:06 +0900

    feat: design goalModel for swift data

---
+++ pacemaker/GoalModel.swift
@@ -0,0 +1,38 @@
+//
+//  GoalModel.swift
+//  Sylva
+//
+//  Created by Lanakee on 4/16/26.
+//
+
+import Foundation
+import SwiftData
+
+enum GoalModelState: Codable {
+  case active
+  case finished
+  case givenUp
+}
+
+@Model
+class GoalModel {
+  @Attribute(.unique) var id: UUID
+  var state: GoalModelState
+  var goal: String
+  
+  var goalBreakdown: [GoalBreakDown]
+  
+  var createdAt: Date
+  var finishedAt: Date?
+  
+  init(goal: String, goals: GoalBreakDown) {
+    self.id = UUID()
+    self.state = .active
+
+    self.goal = goal
+    self.goalBreakdown = [goals]
+
+    self.createdAt = Date()
+    self.finishedAt = nil
+  }
+}
